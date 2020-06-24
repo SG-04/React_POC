3a5eb5f5fe250bbcc9e4d9c28631e0fe684d4e43
Shruti Gupta <shrutigupta.sg33@gmail.com>
2020-06-24 16:03:06 +0530

    Updates

---
+++ precache-manifest.1769ca5a603ea2196eb0c4230e082edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72257dea00f72beecfdd27e4910e02fe",
+    "url": "/React_POC/index.html"
+  },
+  {
+    "revision": "5e66db7d85671ba94130",
+    "url": "/React_POC/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b78832e658b8376e05ba",
+    "url": "/React_POC/static/js/2.f2a5b312.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_POC/static/js/2.f2a5b312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e66db7d85671ba94130",
+    "url": "/React_POC/static/js/main.37f6eabf.chunk.js"
+  },
+  {
+    "revision": "f5f3b223b8c4e02d50c3",
+    "url": "/React_POC/static/js/runtime-main.ac310aaa.js"
+  }
+]);
